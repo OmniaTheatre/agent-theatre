30b32e540931f9f50018ef84bed84274aff50a7f
Ryan Threlfall <ryan@omniatheatre.com>
2023-09-21 22:01:24 -0700

    Killed v0.1

--- WareHouse/OmniaTRaiDER_DefaultOrganization_20230921203721/main.py
+++
@@ -1,9 +0,0 @@
-'''
-Main file for the algorithmic trading application.
-'''
-import tkinter as tk
-from gui import Application
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = Application(root)
-    app.mainloop()
